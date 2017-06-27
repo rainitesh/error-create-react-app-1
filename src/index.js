fbb62abff4ecd6c8d694fef70bba9597a5bc87d0
Andro Babu <rambabusaravanan@gmail.com>
2017-06-27 22:10:34 +0530

    node-thermal-printer added - error occured here

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
+import 'node-thermal-printer'
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
